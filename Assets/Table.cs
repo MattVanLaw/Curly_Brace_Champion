56b76e4ab346a8cfe2684295dc603d90b5389480
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-06-02 00:09:31 -0400

    Sandbox with rough assets and table breaker script

---
+++ Assets/Table.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        rigidBody.isKinematic = false;
+        rigidBody.AddForce(new Vector3(10000f, 10000f, 10000f));
+    }
+}
---
+++ Assets/TableController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableController : MonoBehaviour
+{
+    [SerializeField] GameObject tableLeft;
+    [SerializeField] GameObject tableRight;
+    [SerializeField] GameObject mainCollider;
+
+    [SerializeField] Vector3 explosionLeverRight = new Vector3(1000f, 1000f, 1000f);
+    [SerializeField] Vector3 explosionLeverLeft = new Vector3(-1000f, 1000f, 1000f);
+    Rigidbody tableLeftRigidBody;
+    Rigidbody tableRightRigidBody;
+
+    private void Awake()
+    {
+        tableLeftRigidBody = tableLeft.GetComponent<Rigidbody>();
+        tableRightRigidBody = tableRight.GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tableLeftRigidBody.isKinematic = true;
+        tableRightRigidBody.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        Destroy(mainCollider);
+
+        tableLeftRigidBody.isKinematic = false;
+        tableRightRigidBody.isKinematic = false;
+
+        tableLeftRigidBody.AddForce(explosionLeverLeft);
+        tableRightRigidBody.AddForce(explosionLeverRight);
+    }
+}
